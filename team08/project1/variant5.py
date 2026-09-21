db6068e02001c9d980f34b0563a2df852ba3a35a
CJStriker <calebjudestriker@gmail.com>
2026-09-21 06:23:51 -0400

    Made AI for Var5. It's not perfect but it does win a little more than 50% of the time

--- team08/project1/variant5.py
+++ team08/project1/variant5.py
@@ -14,23 +14,32 @@ sys.path.insert(1, '../teamNN')
 from testcharacter import TestCharacter
 
 # Create the game
-random.seed(123) # TODO Change this if you want different random choices
-g = Game.fromfile('map.txt')
-g.add_monster(StupidMonster("stupid", # name
-                            "S",      # avatar
-                            3, 5,     # position
-))
-g.add_monster(SelfPreservingMonster("aggressive", # name
-                                    "A",          # avatar
-                                    3, 13,        # position
-                                    1             # detection range
-))
+wins = 0
+for i in range(10):
+    
+    g = Game.fromfile('map.txt')
+    g.add_monster(StupidMonster("stupid", # name
+                                "S",      # avatar
+                                3, 5,     # position
+    ))
+    g.add_monster(SelfPreservingMonster("aggressive", # name
+                                        "A",          # avatar
+                                        3, 13,        # position
+                                        1             # detection range
+    ))
 
-# TODO Add your character
-g.add_character(TestCharacter("me", # name
-                              "C",  # avatar
-                              0, 0  # position
-))
+    # TODO Add your character
+    g.add_character(TestCharacter("me", # name
+                                "C",  # avatar
+                                0, 0  # position
+    ))
 
-# Run!
-g.go()
+    # Run!
+    g.go()
+    for event in g.events:
+        print(event)
+        if event.tpe == event.CHARACTER_FOUND_EXIT:
+            wins+=1
+            print("Win")
+
+print("Win/Lose ration: " + str(wins/10))
